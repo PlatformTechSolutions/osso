e2945d353b258ccead2cd59c3f60f8b34d82552c
DILIP KUMAR SHARMA <krdilip276@gmail.com>
2020-07-22 01:15:11 +0530

    Add files via upload

---
+++ precache-manifest.0367f837e99128d03df7e4b205fe6201.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebee8a30bde0ab8d14a6502736ea93d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74a936cce1d4a2ccecd6",
+    "url": "/static/css/3.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "d7c34ac4c10eab4df05e",
+    "url": "/static/css/6.ee241de9.chunk.css"
+  },
+  {
+    "revision": "a0706c3a376b316fdd4a",
+    "url": "/static/css/main.91d8f152.chunk.css"
+  },
+  {
+    "revision": "dd40067a7f4d0fded81e",
+    "url": "/static/js/0.eb2d305f.chunk.js"
+  },
+  {
+    "revision": "d7e36653bcff7999a759",
+    "url": "/static/js/10.fe16dbc8.chunk.js"
+  },
+  {
+    "revision": "85c7f78159599708a72e",
+    "url": "/static/js/11.a4b18ced.chunk.js"
+  },
+  {
+    "revision": "f380666faa77e94ab797",
+    "url": "/static/js/12.7af7246c.chunk.js"
+  },
+  {
+    "revision": "74a936cce1d4a2ccecd6",
+    "url": "/static/js/3.4377d1e1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/3.4377d1e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e2b85ba5ee84e6e9b49",
+    "url": "/static/js/4.166cdfff.chunk.js"
+  },
+  {
+    "revision": "d93c57f863a97af621f7",
+    "url": "/static/js/5.58079ef1.chunk.js"
+  },
+  {
+    "revision": "d7c34ac4c10eab4df05e",
+    "url": "/static/js/6.1bd4a228.chunk.js"
+  },
+  {
+    "revision": "2b5f63988c3d06df1c0a",
+    "url": "/static/js/7.037b183d.chunk.js"
+  },
+  {
+    "revision": "a37cf9d8db928d69dba5",
+    "url": "/static/js/8.6d2c8998.chunk.js"
+  },
+  {
+    "revision": "8a43147ade68501a16fd",
+    "url": "/static/js/9.e666588b.chunk.js"
+  },
+  {
+    "revision": "a0706c3a376b316fdd4a",
+    "url": "/static/js/main.9b32fcf7.chunk.js"
+  },
+  {
+    "revision": "f23b962141532be4efbe",
+    "url": "/static/js/runtime-main.83e5bae5.js"
+  },
+  {
+    "revision": "cae99902b7b53a9bf0af47a113b97f91",
+    "url": "/static/media/badhiya.cae99902.jpg"
+  },
+  {
+    "revision": "0e5f1276297bc81a6d548d7d16b924e0",
+    "url": "/static/media/banner.0e5f1276.jpg"
+  },
+  {
+    "revision": "bd7a0f1efeeaafb3356d37f55898865b",
+    "url": "/static/media/buy2.bd7a0f1e.png"
+  },
+  {
+    "revision": "56ac2d932478bf73d82c9dab1b56a796",
+    "url": "/static/media/default.56ac2d93.png"
+  },
+  {
+    "revision": "3ee321426054e6aa5b3b4a0f26d60e75",
+    "url": "/static/media/deliver.3ee32142.png"
+  },
+  {
+    "revision": "a253d205fe52f21ea2390456a8c92b84",
+    "url": "/static/media/deliver2.a253d205.png"
+  },
+  {
+    "revision": "81b7f56e6fd2f716e4f022d50d3449f4",
+    "url": "/static/media/inspection.81b7f56e.png"
+  },
+  {
+    "revision": "6c5c1dee3e005e3dc7b66db49869a660",
+    "url": "/static/media/location.6c5c1dee.png"
+  },
+  {
+    "revision": "365a776e7fc4a9e25979d012cdc5ffe4",
+    "url": "/static/media/logo.365a776e.jpg"
+  },
+  {
+    "revision": "344383c9946f0dbb7c981749ec99f5a9",
+    "url": "/static/media/search.344383c9.png"
+  }
+]);
